4f519af09ff95a7afd0ce8d1110a640e584d0e79
SD-CodeHub <codehubsd@gmail.com>
2025-12-30 17:29:12 +0530

    added index

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://frontend-assessment.vercel.app"
+  "https://task-assesment-ten.vercel.app/"
 ];
 
 app.use(cors({
